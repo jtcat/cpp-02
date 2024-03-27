79e2909d33277f9c5b7dad53e51b9248bcaf6242
jcat <jftcatarino@gmail.com>
2024-03-27 15:58:22 +0000

    move work

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -1,9 +1,24 @@
 #include "Fixed.h"
 #include <iostream>
+#include <strstream>
+#include <math.h>
 
 Fixed::Fixed(void): _rawbits(0) {
 	std::cout << "Default constructor called" << std::endl;
 }	
+
+Fixed::Fixed(int val) {
+	_rawbits = val << _dec_point;
+}
+
+Fixed::Fixed(float val) {
+	int_float conv = {.f = val};
+	int exponent = FLOAT_EXPONENT(conv.i);
+
+	// Float order of magnitude is too small to be representable
+	if (exponent < -(sizeof(Fixed::_rawbits) * 8 - 1))
+		
+}
 
 Fixed::Fixed(Fixed &ref) {
 	_rawbits = ref._rawbits;
@@ -27,4 +42,38 @@ int	Fixed::getRawBits(void) {
 
 void	Fixed::setRawBits(int const raw) {
 	_rawbits = raw;
+}
+
+int		Fixed::toInt(void) const{
+	return _rawbits >> _dec_point;
+}
+
+float	Fixed::toFloat(void) const{
+	if (_rawbits == 0)
+		return 0.0f;
+	// Write sign
+	int_float conv = {.i = _rawbits & (1 << 31)};
+	// Discards least significant bits
+	if (conv.i)
+		// Number is negative, convert from complement of 2
+		conv.i |= (~_rawbits + 1) >> FLOAT_WIDTH_EXPONENT;
+	else
+		conv.i |= _rawbits >> FLOAT_WIDTH_EXPONENT;
+	// -dec_point is always larger than -32, hence
+	// -dec_point + 127 is always positive, no uint conversion
+	// necessary
+	// Write exponent
+	conv.i |= (-_dec_point + 127) << FLOAT_WIDTH_MANTISSA;
+	return conv.f;
+}
+
+std::ostream&	operator<<(std::ostream& out, const Fixed& num) {
+	if (num._rawbits & (1 << 31)) {
+		out << '-';
+		out << (int)((~num._rawbits + 1) >> num._dec_point);
+	}
+	else
+		out << (int)(num._rawbits >> num._dec_point);
+	out << '.' << (int)(num._rawbits << (32 - num._dec_point) >> num._dec_point);
+	return out;
 }
--- ex01/Fixed.h
+++ ex01/Fixed.h
@@ -1,17 +1,29 @@
 #ifndef FIXED_H
 # define FIXED_H
 
+#include <iostream>
+
+#define FLOAT_MASK_MANTISSA
+#define FLOAT_OFFSET_MANTISSA 9
+#define FLOAT_EXPONENT_BIAS 127
+#define FLOAT_WIDTH_EXPONENT 8
+#define FLOAT_WIDTH_MANTISSA 23
+#define FLOAT_EXPONENT(val) ((val << 1) >> (32 - FLOAT_WIDTH_EXPONENT)) - 127
+
+union int_float {int i; float f;};
+
 class Fixed {
 	private:
 		int					_rawbits;
-		static const int	_frac_bits = 8;
+		static const int	_dec_point = 8;
 	public:
 				Fixed(void);
-				Fixed(const int rawbits);
-				Fixed(const float rawbits);
+				Fixed(const int val);
+				Fixed(const float val);
 				Fixed(Fixed &ref);
 				Fixed &operator=(Fixed &ref);
 				~Fixed(void);
+				friend std::ostream &operator<<(std::ostream &out, const Fixed &num);
 
 		float	toFloat(void)	const;
 		int		toInt(void)	const;
