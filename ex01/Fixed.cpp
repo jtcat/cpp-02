7b43c712520fb6ba241af2c02317e22656cfe789
jcat <jftcatarino@gmail.com>
2024-03-29 00:55:56 +0000

    fix ex01

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -1,33 +1,27 @@
 #include "Fixed.h"
-#include <iostream>
-#include <strstream>
-#include <math.h>
 
 Fixed::Fixed(void): _rawbits(0) {
 	std::cout << "Default constructor called" << std::endl;
 }	
 
-Fixed::Fixed(int val) {
+Fixed::Fixed(const int val) {
+	std::cout << "Int constructor called" << std::endl;
 	_rawbits = val << _dec_point;
 }
 
-Fixed::Fixed(float val) {
-	int_float conv = {.f = val};
-	int exponent = FLOAT_EXPONENT(conv.i);
-
-	// Float order of magnitude is too small to be representable
-	if (exponent < -(sizeof(Fixed::_rawbits) * 8 - 1))
-		
+Fixed::Fixed(const float val) {
+	std::cout << "Float constructor called" << std::endl;
+	_rawbits = roundf(val * (1 << _dec_point));
 }
 
-Fixed::Fixed(Fixed &ref) {
-	_rawbits = ref._rawbits;
+Fixed::Fixed(Fixed const &ref) {
 	std::cout << "Copy constructor called" << std::endl;
+	_rawbits = ref._rawbits;
 }
 
-Fixed &Fixed::operator=(Fixed &ref) {
-	_rawbits = ref._rawbits;
+Fixed&	Fixed::operator=(Fixed const&ref) {
 	std::cout << "Copy assignment operator called" << std::endl;
+	_rawbits = ref._rawbits;
 	return *this;
 }
 
@@ -44,36 +38,16 @@ void	Fixed::setRawBits(int const raw) {
 	_rawbits = raw;
 }
 
-int		Fixed::toInt(void) const{
-	return _rawbits >> _dec_point;
+// _rawbits >> _dec_point "rounds" down, i.e floors.
+// By adding half of a unit we can round up and still be relatively fast
+int		Fixed::toInt(void) const {
+	return (_rawbits + (1 << (_dec_point - 1))) >> _dec_point;
 }
 
-float	Fixed::toFloat(void) const{
-	if (_rawbits == 0)
-		return 0.0f;
-	// Write sign
-	int_float conv = {.i = _rawbits & (1 << 31)};
-	// Discards least significant bits
-	if (conv.i)
-		// Number is negative, convert from complement of 2
-		conv.i |= (~_rawbits + 1) >> FLOAT_WIDTH_EXPONENT;
-	else
-		conv.i |= _rawbits >> FLOAT_WIDTH_EXPONENT;
-	// -dec_point is always larger than -32, hence
-	// -dec_point + 127 is always positive, no uint conversion
-	// necessary
-	// Write exponent
-	conv.i |= (-_dec_point + 127) << FLOAT_WIDTH_MANTISSA;
-	return conv.f;
+float	Fixed::toFloat(void) const {
+	return float(_rawbits) / (1 << _dec_point);
 }
 
 std::ostream&	operator<<(std::ostream& out, const Fixed& num) {
-	if (num._rawbits & (1 << 31)) {
-		out << '-';
-		out << (int)((~num._rawbits + 1) >> num._dec_point);
-	}
-	else
-		out << (int)(num._rawbits >> num._dec_point);
-	out << '.' << (int)(num._rawbits << (32 - num._dec_point) >> num._dec_point);
-	return out;
+	return out << num.toFloat();
 }
--- ex01/Fixed.h
+++ ex01/Fixed.h
@@ -2,15 +2,7 @@
 # define FIXED_H
 
 #include <iostream>
-
-#define FLOAT_MASK_MANTISSA
-#define FLOAT_OFFSET_MANTISSA 9
-#define FLOAT_EXPONENT_BIAS 127
-#define FLOAT_WIDTH_EXPONENT 8
-#define FLOAT_WIDTH_MANTISSA 23
-#define FLOAT_EXPONENT(val) ((val << 1) >> (32 - FLOAT_WIDTH_EXPONENT)) - 127
-
-union int_float {int i; float f;};
+#include <cmath>
 
 class Fixed {
 	private:
@@ -20,10 +12,10 @@ class Fixed {
 				Fixed(void);
 				Fixed(const int val);
 				Fixed(const float val);
-				Fixed(Fixed &ref);
-				Fixed &operator=(Fixed &ref);
+				Fixed(Fixed const &ref);
 				~Fixed(void);
-				friend std::ostream &operator<<(std::ostream &out, const Fixed &num);
+				Fixed&	operator=(Fixed const &ref);
+				friend std::ostream&	operator<<(std::ostream &out, const Fixed &num);
 
 		float	toFloat(void)	const;
 		int		toInt(void)	const;
