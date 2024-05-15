851ea1f00c0c8522807a87cafbfe3ebcd445abcd
jcat <jftcatarino@gmail.com>
2024-05-15 23:28:32 +0100

    tidy header

--- ex01/Fixed.h
+++ ex01/Fixed.h
@@ -8,14 +8,16 @@ class Fixed {
 	private:
 		int					_rawbits;
 		static const int	_dec_point = 8;
+
 	public:
 				Fixed(void);
 				Fixed(const int val);
 				Fixed(const float val);
 				Fixed(Fixed const &ref);
 				~Fixed(void);
-				Fixed&	operator=(Fixed const &ref);
-				friend std::ostream&	operator<<(std::ostream &out, const Fixed &num);
+
+		Fixed&					operator=(Fixed const &ref);
+		friend std::ostream&	operator<<(std::ostream &out, const Fixed &num);
 
 		float	toFloat(void)	const;
 		int		toInt(void)	const;
